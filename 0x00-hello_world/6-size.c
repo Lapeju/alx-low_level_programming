b49b3d64d91b2d2000fa9d51ec9c2bba608ea558
Lapeju <wumytecole0@gmail.com>
2023-09-11 16:42:46 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,4 @@ int main(void)
 	printf("Size of along long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f))
 	return (0);
-};
+}

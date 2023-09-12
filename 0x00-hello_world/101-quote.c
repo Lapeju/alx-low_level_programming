f802ddf20ee9a4d31d6fdb875dfb21ed689cff8f
Lapeju <wumytecole0@gmail.com>
2023-09-11 17:02:33 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A  C program code that prints new line after a required text is printed
+ * Return 1
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}

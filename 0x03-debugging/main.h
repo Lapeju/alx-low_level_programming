274bac186ccf288c935b8fed8addd6aec6e61610
Lapeju <wumytecole0@gmail.com>
2023-09-16 00:04:16 -0700

    3-print_remaining_days.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+#endif
+

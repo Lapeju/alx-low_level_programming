2618bb3a25b232aa009af999371a472fc2da1016
olapeju <wumytecole0@gmail.com>
2023-10-20 01:37:19 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 void first(void) __attribute__ ((constructor));
 
 /**
@@ -10,4 +9,3 @@ void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");}
-
